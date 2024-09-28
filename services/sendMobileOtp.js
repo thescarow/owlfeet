838fb364a1d2c6e988c6727edf4aa51c3f7801ca
Rithik Pathak <rithikpathak123@gmail.com>
2024-09-28 22:27:57 +0000

    improve OTP service

--- services/sendMobileOtp.js
+++ services/sendMobileOtp.js
@@ -1,43 +1,44 @@
-const chalk = require("chalk")
-const dataLog = chalk.blue.bold
-const errorLog = chalk.red.bgWhite.bold
-const mainErrorLog = chalk.white.bgYellow.bold
+const chalk = require("chalk");
+const dataLog = chalk.blue.bold;
+const errorLog = chalk.red.bgWhite.bold;
+const mainErrorLog = chalk.white.bgYellow.bold;
 ////////////////////////////////////////////////////
 if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config()
+  require("dotenv").config();
 }
-const fetch = require("node-fetch")
+const fetch = require("node-fetch");
 exports.sendMobileOtp = async (number, otp) => {
   try {
     // return true
-    console.log("OTP Info:", number, "==>", otp)
+    // console.log("OTP Info:", number, "==>", otp);
 
-    const apiKey = process.env.FAST2SMS_API_KEY
-    const message = "your otp:" + otp + "\nsent by Owlfeet"
+    const apiKey = process.env.FAST2SMS_API_KEY;
+    const message = "your otp:" + otp + "\nsent by Owlfeet";
 
     const response = await fetch("https://www.fast2sms.com/dev/bulkV2", {
       method: "post",
       headers: {
         authorization: apiKey,
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         variables_values: otp,
         route: "otp",
         numbers: number,
-        flash: 0 //1 for flash sms
-      })
-    })
+        flash: 0, //1 for flash sms
+      }),
+    });
 
-    let returnData = await response.json()
-    return returnData.return
+    let returnData = await response.json();
+    // console.log("returndata:", returnData);
+    return returnData.return;
     // console.log("OTP Info:", number, "==>", returnData)
     // return true
   } catch (err) {
-    console.log(errorLog("Error In Sending Mobile OTP:"), mainErrorLog(err))
-    return false
+    console.log(errorLog("Error In Sending Mobile OTP:"), mainErrorLog(err));
+    return false;
   }
-}
+};
 
 // await response.json() ==>
 // {
